41e905e6b818393e3d2538569a351f1fb8e225b5
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-06 15:31:03 +0300

    added the urls and initial views

--- eHostel/hostel/views.py
+++ eHostel/hostel/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    pass 
+
+def student_login(request):
+    pass
+
+def main_page(request):
+    pass 
+
+def hostel_page(request):
+    pass
+
+def search_page(request):
+    pass
