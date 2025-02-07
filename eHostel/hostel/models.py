f323e627aa9d1e543fc678283bba16f185a1b2ec
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-07 14:15:05 +0300

    added the mainpage and adding login

--- eHostel/hostel/models.py
+++ eHostel/hostel/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=10)
     gender = models.CharField(max_length=1, null=True, blank=True)
-    password = models.CharField(max_length=45, null=True, blank=True)
+    password = models.CharField(max_length=255, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
 class Owner(models.Model):
--- eHostel/hostel/urls.py
+++ eHostel/hostel/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', index, name="index"),
     path('student/login', stud_login, name="stud_login"),
-    path('student/signin/<str:type>', signin_student, name="stud_signin")
+    path('student/signin/<str:req_type>', signin_student, name="stud_signin"),
+    path('mainpage/', main_page, name='main_page')
 ]
