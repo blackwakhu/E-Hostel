a63416f60d31cab22357e307adf692ef52156bcd
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-07 23:20:19 +0300

    added hostel

--- EHostel/EHostel/settings.py
+++ EHostel/EHostel/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'EHostel.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Hostel',
+        'USER': 'shibero',
+        'PASSWORD': 'wakhu',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
