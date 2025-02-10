f119fd3e464bca3ce6fdd591ec79eef6065e7893
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-10 11:27:58 +0300

    can now log in a student from the student login to the main page

--- EHostel/hostel/views.py
+++ EHostel/hostel/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.hashers import make_password, check_password
 
 from .models import *
@@ -15,7 +15,15 @@ def stud_login(request):
 def stud_log(request, req_type):
     if request.method == "POST":
         if req_type == 'signin':
-            pass 
+            admission_number = request.POST.get("adminNumberin")
+            password = request.POST.get("passwdin")
+            student = get_object_or_404(Student, pk=admission_number)
+            print("gotten the student")
+            print(student.first_name)
+            print(student.password)
+            if check_password(password, student.password):
+                print("inside the password checker")
+                return redirect('main_page')
         elif req_type == 'signup':
             admission_number = request.POST.get('adminNumber')
             first_name = request.POST.get('fName')
