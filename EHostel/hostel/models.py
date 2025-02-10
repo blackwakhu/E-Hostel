30df6b71aba8d5d21f3869e5ad723d3ec8ceb62d
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-10 12:25:21 +0300

    can now add and login a hostel owner

--- EHostel/hostel/models.py
+++ EHostel/hostel/models.py
@@ -11,3 +11,11 @@ class Student(models.Model):
     gender = models.CharField(max_length=1, null=True, blank=True)
     password = models.CharField(max_length=255, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+
+class Owner(models.Model):
+    username = models.CharField(primary_key=True, max_length=25)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=10, unique=True)
+    password = models.CharField(max_length=255)
