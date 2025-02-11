0063c113ff6bcdde1b75b52dd764e1fef6683120
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-11 11:08:30 +0300

    added the rest of the models

--- EHostel/EHostel/settings.py
+++ EHostel/EHostel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
--- EHostel/hostel/models.py
+++ EHostel/hostel/models.py
@@ -25,4 +25,50 @@ class Owner(models.Model):
 
     def __str__(self):
         return self.username
+
+class Hostel(models.Model):
+    hostel_name = models.CharField(max_length=25)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    price_per_month = models.IntegerField()
+    location = models.CharField(max_length=45)
+    number_rooms = models.IntegerField(default=1, null=True, blank=True)
+    room_type = models.CharField(max_length=45, default='Bedsitter')
+    available_rooms = models.IntegerField(default=0)
+    county = models.CharField(max_length=45, null=True, blank=True)
+    town = models.CharField(max_length=45, null=True, blank=True)
+    locality = models.CharField(max_length=45, null=True, blank=True)
+    
+    class Meta:
+        unique_together = ('owner', 'hostel_name')
+
+class HostelAmenities(models.Model):
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    amenity = models.CharField(max_length=45, null=True, blank=True)
+
+class HostelImages(models.Model):
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="hostel_images/", null=True, blank=True)        
+
+class Booking(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=45, default='Pending')
+    payment_status = models.CharField(max_length=45, default='Pending')
+    
+    class Meta:
+        unique_together = ('student', 'hostel', 'created_at')
+
+class Review(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    comment = models.TextField(null=True, blank=True)
+    rating = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Payment(models.Model):
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length=45, primary_key=True)
+    amount = models.CharField(max_length=45, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)    
     
