8a90046bf1d9a32aea7c00aab2da66209547c177
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-02-25 12:00:57 +0300

    added for phone number

--- EHostel/hostel/static/hostel/js/login.js
+++ EHostel/hostel/static/hostel/js/login.js
@@ -1,28 +1,44 @@
 var passwdOwner = document.querySelector("#passwdOwner");
 var passwd1Owner = document.querySelector("#passwd1Owner");
 var submitOwner = document.querySelector("#submitOwnerBtn");
+var telOwner = document.querySelector("#phone_number_owner");
 var msgElement = document.querySelector("#message");
 var msgConfElement = document.querySelector("#messageConf");
+var msgTellElement = document.querySelector("#messageTell");
 function validatePassword(password) {
     var regex = /^(?=.*[A-Za-z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return regex.test(password);
 }
-function checkPasswords(original, confirm, submit, msg, msgConf) {
+function validateNumber(num) {
+    var regex = /^(?:\+?254)?(?:0|7|1)(?:(?:[1-9][0-9]{7})|(?:4[0-9]{6}))$/;
+    return regex.test(num);
+}
+function checkPasswords(original, confirm, tellinp, submit, msg, msgConf, msgTell) {
     var password = original.value;
     var password1 = confirm.value;
+    var tell = tellinp.value;
     var isValid = validatePassword(password);
     var passmatch = password === password1;
+    var validTell = validateNumber(tell);
     if (!isValid) {
-        msg.textContent = "Password must be at least 8 characters long, contain a letter, a number, and a symbol.";
+        msg.textContent = "Password must be at least 8 characters long, contain a letter, a number, and a symbol.❌";
         msg.style.color = "red";
     }
     else {
-        msg.textContent = "Strong password ✅";
+        msg.textContent = "Strong password ";
         msg.style.color = "green";
+    }
+    if (!validTell) {
+        msgTell.textContent = "Invalid phone number. Must start with a +254 or 0 followed by 8 numbers❌";
+        msgTell.style.color = "red";
+    }
+    else {
+        msgTell.textContent = "Correct Number";
+        msgTell.style.color = "green";
     }
     if (password1.length > 0) {
         if (passmatch) {
-            msgConf.textContent = "Passwords match ✅";
+            msgConf.textContent = "Passwords match ";
             msgConf.style.color = "green";
         }
         else {
@@ -33,11 +49,14 @@ function checkPasswords(original, confirm, submit, msg, msgConf) {
     else {
         msgConf.textContent = "";
     }
-    submit.disabled = !(isValid && passmatch);
+    submit.disabled = !(isValid && passmatch && validTell);
 }
 passwdOwner.addEventListener("input", function () {
-    checkPasswords(passwdOwner, passwd1Owner, submitOwner, msgElement, msgConfElement);
+    checkPasswords(passwdOwner, passwd1Owner, telOwner, submitOwner, msgElement, msgConfElement, msgTellElement);
 });
 passwd1Owner.addEventListener("input", function () {
-    checkPasswords(passwdOwner, passwd1Owner, submitOwner, msgElement, msgConfElement);
+    checkPasswords(passwdOwner, passwd1Owner, telOwner, submitOwner, msgElement, msgConfElement, msgTellElement);
+});
+telOwner.addEventListener("input", function () {
+    checkPasswords(passwdOwner, passwd1Owner, telOwner, submitOwner, msgElement, msgConfElement, msgTellElement);
 });
--- EHostel/hostel/static/hostel/js/login.ts
+++ EHostel/hostel/static/hostel/js/login.ts
@@ -1,31 +1,49 @@
 let passwdOwner: HTMLInputElement = document.querySelector<HTMLInputElement>("#passwdOwner")
 let passwd1Owner: HTMLInputElement = document.querySelector<HTMLInputElement>("#passwd1Owner")
 let submitOwner: HTMLInputElement = document.querySelector<HTMLInputElement>("#submitOwnerBtn")
+let telOwner: HTMLInputElement = document.querySelector<HTMLInputElement>("#phone_number_owner") 
+
 let msgElement: HTMLSpanElement = document.querySelector<HTMLSpanElement>("#message")
 let msgConfElement: HTMLSpanElement = document.querySelector<HTMLSpanElement>("#messageConf")
+let msgTellElement: HTMLSpanElement = document.querySelector<HTMLSpanElement>("#messageTell")
 
 function validatePassword(password: string): boolean {
     const regex = /^(?=.*[A-Za-z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     return regex.test(password)
 }
 
-function checkPasswords(original: HTMLInputElement, confirm: HTMLInputElement, submit: HTMLInputElement, msg: HTMLSpanElement, msgConf: HTMLSpanElement) {
+function validateNumber(num: string): boolean {
+    const regex = /^(?:\+?254)?(?:0|7|1)(?:(?:[1-9][0-9]{7})|(?:4[0-9]{6}))$/
+    return regex.test(num)
+}
+
+function checkPasswords(original: HTMLInputElement, confirm: HTMLInputElement, tellinp: HTMLInputElement, submit: HTMLInputElement, msg: HTMLSpanElement, msgConf: HTMLSpanElement, msgTell: HTMLSpanElement) {
     const password: string = original.value
     const password1: string = confirm.value
+    const tell: string = tellinp.value
     const isValid: boolean = validatePassword(password)
     const passmatch: boolean = password === password1
+    const validTell: boolean = validateNumber(tell)
 
     if (!isValid) {
-        msg.textContent = "Password must be at least 8 characters long, contain a letter, a number, and a symbol.";
+        msg.textContent = "Password must be at least 8 characters long, contain a letter, a number, and a symbol.❌";
         msg.style.color = "red";
     } else {
-        msg.textContent = "Strong password ✅";
+        msg.textContent = "Strong password ";
         msg.style.color = "green";
+    }
+
+    if (!validTell) {
+        msgTell.textContent = "Invalid phone number. Must start with a +254 or 0 followed by 8 numbers❌";
+        msgTell.style.color = "red";
+    } else {
+        msgTell.textContent = "Correct Number";
+        msgTell.style.color = "green";
     }
 
     if (password1.length > 0) {
         if (passmatch) {
-            msgConf.textContent = "Passwords match ✅";
+            msgConf.textContent = "Passwords match ";
             msgConf.style.color = "green";
         } else {
             msgConf.textContent = "Passwords do not match ❌";
@@ -34,17 +52,19 @@ function checkPasswords(original: HTMLInputElement, confirm: HTMLInputElement, s
     } else {
         msgConf.textContent = "";
     }
-    submit.disabled = !(isValid && passmatch)
+    submit.disabled = !(isValid && passmatch && validTell)
 
 }
 
 
 passwdOwner.addEventListener("input", () => {
-    checkPasswords(passwdOwner, passwd1Owner, submitOwner, msgElement, msgConfElement)
+    checkPasswords(passwdOwner, passwd1Owner, telOwner, submitOwner, msgElement, msgConfElement, msgTellElement)
 })
 passwd1Owner.addEventListener("input", () => {
-    checkPasswords(passwdOwner, passwd1Owner, submitOwner, msgElement, msgConfElement)
+    checkPasswords(passwdOwner, passwd1Owner, telOwner, submitOwner, msgElement, msgConfElement, msgTellElement)
 })
-
+telOwner.addEventListener("input", () => {
+    checkPasswords(passwdOwner, passwd1Owner, telOwner, submitOwner, msgElement, msgConfElement, msgTellElement)
+})
 
 
