b7826d84ff184cc68163dfca44c4ed3da4903330
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-03-03 12:05:38 +0300

    can do a few things

--- EHostel/hostel/static/hostel/js/owner.js
+++ EHostel/hostel/static/hostel/js/owner.js
@@ -1,4 +1,4 @@
-import { hideDivElements, hideUrlDivElements } from "./mymodules.js";
+import { hideDivElements, hideUrlDivElements, hideEditElements, url, validateNumber } from "./mymodules.js";
 let home_btn = document.querySelector(".home");
 let hostels_btn = document.querySelector(".hostels");
 let new_hostel_btn = document.querySelector(".new-hostel");
@@ -13,6 +13,44 @@ let btnObj = new Array();
 for (let i = 0; i < divElements.length; i++) {
     btnObj.push({ "button": buttonElements[i], "div": divElements[i] });
 }
+let updateElementsOwner = [
+    {
+        editBtn: document.querySelector("#owner-fname-btn"),
+        displayClass: document.querySelector(".owner-fname-display"),
+        inputClass: document.querySelector(".owner-fname-input"),
+        cancelBtn: document.querySelector("#owner_fname_cancel"),
+        inputElem: document.querySelector("#owner-fname-inp"),
+        subbtn: document.querySelector("#owner-fname-sub"),
+        column: "first_name"
+    },
+    {
+        editBtn: document.querySelector("#owner-lname-btn"),
+        displayClass: document.querySelector(".owner-lname-display"),
+        inputClass: document.querySelector(".owner-lname-input"),
+        cancelBtn: document.querySelector("#owner_lname_cancel"),
+        inputElem: document.querySelector("#owner-lname-inp"),
+        subbtn: document.querySelector("#owner-lname-sub"),
+        column: "last_name"
+    },
+    {
+        editBtn: document.querySelector("#owner-email-btn"),
+        displayClass: document.querySelector(".owner-email-display"),
+        inputClass: document.querySelector(".owner-email-input"),
+        cancelBtn: document.querySelector("#owner_email_cancel"),
+        inputElem: document.querySelector("#owner-email-inp"),
+        subbtn: document.querySelector("#owner-email-sub"),
+        column: "email"
+    },
+    {
+        editBtn: document.querySelector("#owner-contact-btn"),
+        displayClass: document.querySelector(".owner-contact-display"),
+        inputClass: document.querySelector(".owner-contact-input"),
+        cancelBtn: document.querySelector("#owner_contact_cancel"),
+        inputElem: document.querySelector("#owner-contact-inp"),
+        subbtn: document.querySelector("#owner-contact-sub"),
+        column: "phone_number"
+    }
+];
 const uname = document.querySelector("#owner-uname").textContent;
 document.addEventListener("DOMContentLoaded", () => {
     btnObj.forEach((bObj) => {
@@ -23,4 +61,19 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
     hideUrlDivElements(home_div, divElements);
+    updateElementsOwner.forEach((elem) => {
+        elem.editBtn.addEventListener("click", () => { hideEditElements(elem.editBtn, elem.displayClass, elem.inputClass, elem.cancelBtn, "hide-div"); });
+        elem.cancelBtn.addEventListener("click", () => { hideEditElements(elem.cancelBtn, elem.inputClass, elem.displayClass, elem.editBtn, "hide-div"); });
+        elem.subbtn.addEventListener("click", () => {
+            // saveData(elem.inputElem)
+            if (elem.column === "phone_number" && !(validateNumber(elem.inputElem.value))) {
+                alert("The phone number is not valid");
+            }
+            else {
+                const elemurl = `${url}/api/student/update/${uname}/${elem.column}/`;
+                // handleUpdateClick(elemurl, elem.column, elem.inputElem.value, elem.displayClass)
+                hideEditElements(elem.cancelBtn, elem.inputClass, elem.displayClass, elem.editBtn, "hide-div");
+            }
+        });
+    });
 });
--- EHostel/hostel/static/hostel/js/owner.ts
+++ EHostel/hostel/static/hostel/js/owner.ts
@@ -1,4 +1,4 @@
-import { hideDivElements, hideUrlDivElements } from "./mymodules.js"
+import { hideDivElements, hideUrlDivElements, UpdatedElementsint, hideEditElements, url, validateNumber, handleUpdateClick } from "./mymodules.js"
 
 let home_btn: HTMLAnchorElement = document.querySelector<HTMLAnchorElement>(".home")
 let hostels_btn: HTMLAnchorElement = document.querySelector<HTMLAnchorElement>(".hostels")
@@ -19,6 +19,45 @@ for (let i = 0; i < divElements.length; i++) {
     btnObj.push({"button": buttonElements[i], "div": divElements[i]})
 }
 
+let updateElementsOwner: UpdatedElementsint[] = [
+    {
+        editBtn: document.querySelector("#owner-fname-btn"),
+        displayClass: document.querySelector(".owner-fname-display"),
+        inputClass: document.querySelector(".owner-fname-input"),
+        cancelBtn: document.querySelector("#owner_fname_cancel"),
+        inputElem: document.querySelector("#owner-fname-inp"),
+        subbtn: document.querySelector("#owner-fname-sub"),
+        column: "first_name"
+    },
+    {
+        editBtn: document.querySelector("#owner-lname-btn"),
+        displayClass: document.querySelector(".owner-lname-display"),
+        inputClass: document.querySelector(".owner-lname-input"),
+        cancelBtn: document.querySelector("#owner_lname_cancel"),
+        inputElem: document.querySelector("#owner-lname-inp"),
+        subbtn: document.querySelector("#owner-lname-sub"),
+        column: "last_name"
+    },
+    {
+        editBtn: document.querySelector("#owner-email-btn"),
+        displayClass: document.querySelector(".owner-email-display"),
+        inputClass: document.querySelector(".owner-email-input"),
+        cancelBtn: document.querySelector("#owner_email_cancel"),
+        inputElem: document.querySelector("#owner-email-inp"),
+        subbtn: document.querySelector("#owner-email-sub"),
+        column: "email"
+    },
+    {
+        editBtn: document.querySelector("#owner-contact-btn"),
+        displayClass: document.querySelector(".owner-contact-display"),
+        inputClass: document.querySelector(".owner-contact-input"),
+        cancelBtn: document.querySelector("#owner_contact_cancel"),
+        inputElem: document.querySelector("#owner-contact-inp"),
+        subbtn: document.querySelector("#owner-contact-sub"),
+        column: "phone_number"
+    }
+]
+
 const uname: string = document.querySelector("#owner-uname").textContent
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -31,5 +70,19 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     hideUrlDivElements(home_div, divElements)
+    updateElementsOwner.forEach((elem) => {
+            elem.editBtn.addEventListener("click", () => { hideEditElements(elem.editBtn, elem.displayClass, elem.inputClass, elem.cancelBtn, "hide-div") })
+            elem.cancelBtn.addEventListener("click", () => { hideEditElements(elem.cancelBtn, elem.inputClass, elem.displayClass, elem.editBtn, "hide-div") })
+            elem.subbtn.addEventListener("click", () => {
+                // saveData(elem.inputElem)
+                if (elem.column === "phone_number" && !(validateNumber(elem.inputElem.value))) {
+                    alert("The phone number is not valid")
+                } else {
+                    const elemurl: string = `${url}/api/student/update/${uname}/${elem.column}/`
+                    // handleUpdateClick(elemurl, elem.column, elem.inputElem.value, elem.displayClass)
+                    hideEditElements(elem.cancelBtn, elem.inputClass, elem.displayClass, elem.editBtn, "hide-div")
+                }
+            })
+        })
    
 })
